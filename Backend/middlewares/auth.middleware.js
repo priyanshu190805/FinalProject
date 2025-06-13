3ce59fbee98dae8e0f579d069af111098b17c292
Priyanshu singh <priyanshu.singh.1908@gmail.com>
2025-06-13 19:19:32 +0530

    Update auth.middleware.js

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -29,7 +29,7 @@ const authUser = async (req, res, next) => {
 }
 
 const authCaptain = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    const token = req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
 
     if(!token){
         return res.status(401).json({message : "Unauthorized"})
